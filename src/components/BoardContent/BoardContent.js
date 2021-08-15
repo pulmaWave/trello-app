00433063cf88715b1f2ad76ff8b6b118b8888aef
Nguyen Cong Thinh <congthinhq@gmail.com>
2021-08-15 16:11:50 +0700

    Implement drag and drop, setup eslint

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Container, Draggable } from 'react-smooth-dnd'
 import './BoardContent.scss'
 import { isEmpty } from 'lodash'
 
@@ -7,34 +8,62 @@ import { initialData } from 'actions/InitialData'
 import { mapOrder } from 'utilities/sort'
 
 const BoardContent = () => {
-    const [board, setBoard] = useState({})
-    const [columns, setColumns] = useState([])
+  const [board, setBoard] = useState({})
+  const [columns, setColumns] = useState([])
+  const onColumnDrop = (dropResult) => {
+    console.log(dropResult)
+  }
 
-    useEffect(() => {
-        const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
-        if (boardFromDB) {
-            setBoard(boardFromDB)
-
-            setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
-        }
-    }, [])
-
-    if (isEmpty(board)) {
-        return <div className="not-found" style={{ textAlign: "center", color: "white", display: "flex", justifyContent: "center", alignItems: "center," }}>
-            <h1>
-                Board not found!
-            </h1>
-        </div>
-    }
-
-    return (
-        <nav className="board-content">
-            {columns.map((column, index) => {
-                return <Column key={index} column={column} />
-            })}
-        </nav>
+  useEffect(() => {
+    const boardFromDB = initialData.boards.find(
+      (board) => board.id === 'board-1'
     )
+    if (boardFromDB) {
+      setBoard(boardFromDB)
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
+    }
+  }, [])
+
+  if (isEmpty(board)) {
+    return (
+      <div
+        className="not-found"
+        style={{
+          textAlign: 'center',
+          color: 'white',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center,'
+        }}
+      >
+        <h1>Board not found!</h1>
+      </div>
+    )
+  }
+
+  return (
+    <nav className="board-content">
+      <Container
+        orientation="horizontal"
+        onDrop={onColumnDrop}
+        getChildPayload={(index) => columns[index]}
+        dragHandleSelector=".column-drag-handle"
+        dropPlaceholder={{
+          animationDuration: 150,
+          showOnTop: true,
+          className: 'column-drop-preview'
+        }}
+      >
+        {columns.map((column, index) => {
+          return (
+            <Draggable key={index}>
+              <Column column={column} />
+            </Draggable>
+          )
+        })}
+      </Container>
+    </nav>
+  )
 }
 
 export default BoardContent
-
